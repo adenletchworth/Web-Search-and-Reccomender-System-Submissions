c66cc06110e66e6f2e6e8dddccb54ae88eb34989
adenletchworth <adenrletchworth@gmail.com>
2024-02-06 12:27:53 -0800

    feat: finish matmul, tf and idf functions

--- Assignment 1/indexing.py
+++ Assignment 1/indexing.py
@@ -1,15 +1,15 @@
 #-------------------------------------------------------------------------
 # AUTHOR: Aden Letchworth
 # FILENAME: indexing.py
-# SPECIFICATION: description of the program
+# SPECIFICATION: Simple Data Processing for Document into TF-IDF Matrix
 # FOR: CS 4250- Assignment #1
-# TIME SPENT: how long it took you to complete the assignment
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard arrays
 
 #Importing some Python libraries
 import csv
+import math # for log function
 
 documents = []
 labels = []
@@ -73,11 +73,11 @@ documents = stem(documents)
 #Identifying the index terms.
 #--> add your Python code here
 def get_terms(docs):
-  terms = set()
+  terms = []
   for doc in docs:
     for word in doc:
       if word not in terms:
-        terms.add(word)
+        terms.append(word)
   return terms
 
 terms = get_terms(documents)
@@ -88,24 +88,54 @@ def get_tf(terms, docs):
   tf = []
   for doc in docs:
       num_words = len(doc)
-      tf_dict = {}
-      for term in terms:
-          tf_dict[term] = doc.count(term) / num_words if term in doc else 0
-      tf.append(tf_dict)
+      tf_doc = [0] * len(terms)
+      for i, term in enumerate(terms):
+          tf_doc[i] = doc.count(term) / num_words if term in doc else 0
+      tf.append(tf_doc)
   return tf
+
       
 
 def get_idf(terms, docs):
-  return
+  idf = []
+  n = len(docs)
+  for term in terms:
+    count = 0
+    for doc in docs:
+      if term in doc:
+        count += 1
+    idf.append(math.log(n/count, 10) if count != 0 else 0) 
+  return idf
+      
 
 def matmul(tf, idf):
-  return 
+  doc_term_matrix = []
+  
+  # Ensure the number of terms in TF matches the number of terms in IDF
+  if len(tf[0]) != len(idf):
+      return
+
+  # Iterate over each document
+  for doc in tf:
+      doc_terms = []
+      # Multiply each term's TF with its corresponding IDF
+      for term_idx, tf_value in enumerate(doc):
+          tfidf_value = tf_value * idf[term_idx]
+          doc_terms.append(tfidf_value)
+      doc_term_matrix.append(doc_terms)
+  
+  return doc_term_matrix
 
 tf = get_tf(terms, documents)
-print(tf)
 idf = get_idf(terms, documents)
 
 doc_term_matrix = matmul(tf, idf)
 
 #Printing the document-term matrix.
 #--> add your Python code here
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(" ".join(f"{elem:.2f}" for elem in row))
+
+print_matrix(doc_term_matrix)
